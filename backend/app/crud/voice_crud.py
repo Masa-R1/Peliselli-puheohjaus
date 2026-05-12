3acd6380e1fa35ca7dde382b1117ad485b48de2b
sb16353 <sb16353@student.samk.fi>
2026-05-12 08:56:30 +0300

    voice-endpointit

---
+++ backend/app/crud/voice_crud.py
@@ -0,0 +1,8 @@
+__voice_enabled = False
+
+def is_voice_enabled():
+    return __voice_enabled;
+
+def set_voice_enabled(enabled: bool):
+    global __voice_enabled
+    __voice_enabled = enabled
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import prompts
+from .routers import prompts, voice
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,3 +13,4 @@ app.add_middleware(
 )
 
 app.include_router(prompts.router)
+app.include_router(voice.router)
