538cf0d148d404615fad1db67dfdc3886aec018c
Juha Hurri <juhah60@gmail.com>
2026-05-22 11:54:46 +0300

    Testausta

--- backend/app/example.py
+++ backend/app/example.py
@@ -1,20 +1,28 @@
-from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from elevenlabs.play import play
 import os
-os.environ['PATH'] += os.pathsep + 'C:/Users/Juha/Downloads/ffmpeg-8.1.1-full_build/ffmpeg-8.1.1-full_build/bin'
+from dotenv import load_dotenv
+import websockets
 
 load_dotenv()
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
-elevenlabs = ElevenLabs(
-    api_key=os.getenv("ELEVENLABS_API_KEY"),
-)
+voice_id = 'Xb7hH8MSUJpSbSDYk0k2'
 
-audio = elevenlabs.text_to_speech.convert(
-    text="Lorem ipsum dolor sit amet. Myrky on minun lempi väri ja se on aina hyvä valinta.",
-    voice_id="CwhRBWXzGAHq8TQ4Fs17",
-    model_id="eleven_flash_v2_5",
-    output_format="mp3_44100_128",
-)
+model_id = 'eleven_flash_v2_5'
 
-play(audio)
+async def text_to_speech_ws_streaming(voice_id, model_id):
+    uri = f"wss://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream-input?model_id={model_id}"
+
+    async with websockets.connect(uri) as websocket:
+    
+    async def text_to_speech_ws_streaming(voice_id, model_id):
+        async with websockets.connect(uri) as websocket:
+            await websocket.send(json.dumps({
+                "text": " ",
+                "voice settings": {"stability" : 0.5, "similarity_boost": 0.8, "use_speaker_boost": False},
+                "generation_config": {
+                    "chunk_length_schedule": [120, 160, 250, 290]
+                },
+                "xi_api_key": ELEVENLABS_API_KEY,
+            }))
+
+            text = "The twilight sun cast its "
