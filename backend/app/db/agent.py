b9effc3891021dc2daeadc96a6e0e7030417442e
sb16353 <sb16353@student.samk.fi>
2026-05-25 13:01:42 +0300

    Muutoksia 2

--- backend/app/db/agent.py
+++ backend/app/db/agent.py
@@ -108,7 +108,7 @@ def change_ha_light_color(color: str) -> str:
     """English: Tool to change the light color in Home Assistant. Input color can be in whatever language but has to be returned in English. 
     Finnish: Työkalu, jolla voi vaihtaa valon väriä Home Assistantissa. Palauta aina väri Englanniksi riippumatta syötekielestä.
     Args:
-        color: The name of the color to change to. Should be in English."""
+        color: The name of the color to change to. Should be returned in English, but can be in any language in the input."""
     print(color)
     return f"Changed light color to {color}."
 
@@ -118,7 +118,7 @@ def change_ha_scene(scene: str) -> str:
     Finnish: Työkalu, jolla voi vaihtaa sceneä Home Assistantissa.
     
     Args:
-        scene: The name of the scene to change to. Should be in English."""
+        scene: The name of the scene to change to. Should be returned in English, but can be in any language in the input."""
     print(scene)
     return f"Changed scene to {scene}."
 
