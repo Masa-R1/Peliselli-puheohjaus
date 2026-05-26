fe0102191672153766f09430e9f74e685f2b5148
sb16353 <sb16353@student.samk.fi>
2026-05-26 09:30:08 +0300

    tool parannuksia

--- backend/app/db/agent.py
+++ backend/app/db/agent.py
@@ -1,3 +1,5 @@
+import random
+
 import httpx
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
@@ -143,23 +145,45 @@ def get_current_lunch_at_samk_silvia() -> str:
         return json.dumps({"ErrorText": f"Failed to fetch lunch menu: {exc}"}, ensure_ascii=False)
 
 @tool
-def get_chuck_norris_joke() -> str:
-    """English: Tool to get a random Chuck Norris joke.
-    Finnish: Työkalu satunnaisen Chuck Norris -vitsin hakemiseen.
+def get_chuck_norris_joke(category: str = str(), query: str = str()) -> str:
+    """English: Tool to get a Chuck Norris joke.
+    Finnish: Työkalu Chuck Norris -vitsin hakemiseen.
 
     Returns the joke text from the `value` field in the API response.
+
+        Args:
+            category: The category of the random Chuck Norris joke to retrieve.
+            query: The query by which to get a Chuck Norris joke. If empty, returns a random joke. If a category is given, it takes priority over the query.
+
     """
+    MISSING_FIELD = "Chuck Norris doesn’t check for missing JSON fields. The JSON adds them out of fear."
+
     url = "https://api.chucknorris.io/jokes/random"
     try:
-        response = httpx.get(url, timeout=httpx.Timeout(AGENT_TIMEOUT))
+        params = dict()
+
+        if category and category != "":
+            params.append(("category", category))
+        elif query and query != "":
+            url = "https://api.chucknorris.io/jokes/search"
+            params.append(("query", query))
+
+        response = httpx.get(url, timeout=httpx.Timeout(AGENT_TIMEOUT), params=params)
         response.raise_for_status()
         payload = response.json()
 
         joke = payload.get("value")
         if joke:
             return str(joke)
+        else:
+            jokes = payload.get("result", [])
+            if jokes and isinstance(jokes, list) and len(jokes) > 0: 
+                joke = jokes[random.randint(0, len(jokes) - 1)].get("value", MISSING_FIELD)
+                return str(joke)
+            else:
+                return get_chuck_norris_joke()
 
-        return "Chuck Norris doesn’t check for missing JSON fields. The JSON adds them out of fear."
+        return MISSING_FIELD
     except Exception as exc:
         return f"Chuck Norris once sent a GET request for a Chuck Norris joke. The endpoint returned {exc}—because the server couldn’t handle the pressure of retrieving Chuck Norris."
 
